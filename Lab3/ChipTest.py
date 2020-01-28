c705d13487f0d33987bff744d45260e8c0fa36a2
Anmol Goyal <anmolgoyal@gmail.com>
2020-01-28 15:08:25 +0530

    Recursive chip test algorithm

--- Lab3/ChipTest.py
+++ Lab3/ChipTest.py
@@ -1,7 +1,28 @@
 import random
 
 def Check(r1,r2):
-    return r1 +  r2
+    number = [-1,1]
+    if  r1 + r2 == 2 :  
+        return 2 
+    elif r1 + r2 == - 2 : 
+        return random.choice(number) +  random.choice(number)
+
+    return random.choice(number) - 1
+
+def Recursive(Data,Number):
+    number = [0,1]
+    Temp =  []
+    Id = []
+
+    for i in range(0,len(Data),2):
+        if i == len(Data) - 1  :  
+            continue
+        else:
+            a  =  random.choice(number)
+            Temp.append(Data[i+a])
+            Id.append(Number[i+a])
+    
+    return Temp,Id
 
 TestData =  [-1,-1,-1,1,1,1,1,1,1,1]
 
@@ -17,23 +38,35 @@ for i in range(0,len(TestData),2):
         Temp.append(TestData[i+a])
         Id.append(i+1+a)
         
+# print(Temp)
+# print(Id)
 
-while len(Temp) !=  1 :
-    i = 0  
-    duplicate  = []
-    Id_duplicate = []
-
-    
-    if Check(Temp[i],Temp[i+1]) == 2 : 
-        #print(random.choice(number),TestData[i],TestData[i+1])
-        b = random.choice(number)
-        duplicate.append(Temp[i+b])
-        Id_duplicate.append(Id[i+1+b])
+while len(Temp) != 1  : 
+    Temp,Id =  Recursive(Temp,Id)
 
 
-    Temp = duplicate
-    Id =  Id_duplicate
-    i +=  2  
+# print(Temp)
+# print(Id)
+
+
+# while len(Temp) !=  1 :
+      
+#     duplicate  = []
+#     Id_duplicate = []
+
+#     for i in range(0,len(Temp),2):
+#         if  i == len(TestData) - 1 or i+1 > len(TestData) :  
+#             continue
+#         if Check(Temp[i],Temp[i+1]) == 2 : 
+#         #print(random.choice(number),TestData[i],TestData[i+1])
+#             b = random.choice(number)
+#             duplicate.append(Temp[i+b])
+#             Id_duplicate.append(Id[i+1+b])
+
+
+#     Temp = duplicate
+#     Id =  Id_duplicate
+ 
 
 print("{} th Robot is correctly identified. \n".format(Id[0]))
 
